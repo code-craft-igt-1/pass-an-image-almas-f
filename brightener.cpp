4a571352bd5e370455f3a1a182eb71df5eeb0ccb
almas-f <62561466+almas-f@users.noreply.github.com>
2024-11-07 23:45:23 +0530

    Update brightener.cpp

--- brightener.cpp
+++ brightener.cpp
@@ -4,7 +4,26 @@ using namespace std;
 ImageBrightener::ImageBrightener(Image& inputImage) : m_inputImage(inputImage) {
 }
 
-int ImageBrightener::BrightenWholeImage() {
+ // Brightens the whole image and returns the number of attenuated pixels
+int ImageBrightener::BrightenWholeImage() {        
+	int attenuatedCount = 0;
+
+        for (int i = 0; i < m_inputImage.rows; ++i) {
+            for (int j = 0; j < m_inputImage.columns; ++j) {
+                int index = i * m_inputImage.columns + j; // Calculate pixel index
+                uint8_t& pixel = m_inputImage.pixels[index]; // Access pixel
+
+                // Brighten pixel (increase its value)
+                if (pixel < 255) {
+                    pixel += 10; // Increase brightness by 10 (capped at 255)
+                    ++attenuatedCount;
+                }
+            }
+        }
+
+        return attenuatedCount;
+    }
+/*int ImageBrightener::BrightenWholeImage() {
     // For brightening, we add a certain grayscale (25) to every pixel.
     // While brightening, some pixels may cross the max brightness. They are
     // called 'attenuated' pixels
@@ -24,7 +43,7 @@ int ImageBrightener::BrightenWholeImage() {
     }
     
     return attenuatedPixelCount;
-}
+}*/
 
 
 Image ImageBrightener::GetImage() {
